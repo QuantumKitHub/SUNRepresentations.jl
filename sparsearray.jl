4bb45d6f2c676fc0aaba7ef41ccdf4c0623552f2
maarten <maarten.vandamme@ugent.be>
2020-09-11 10:27:35 +0200

    mwe

--- sparsearray.jl
+++ sparsearray.jl
@@ -1,38 +1,37 @@
-using TensorOperations
+using TensorOperations,SparseArrays
 import LinearAlgebra;
 
-#=
-Couldn't find this silly thing anywhere
-It's not fast, it's not fancy, but it works ...
-=#
-struct SparseDOK{Elt,N}# <: AbstractArray{Elt,N}
-    eldict::Dict{NTuple{N,Int64},Elt}
-    sizes::NTuple{N,Int64}
+struct SparseDOK{Elt,N} <: AbstractSparseArray{Elt,Int,N}
+    eldict::Dict{NTuple{N,Int},Elt}
+    sizes::NTuple{N,Int}
 end
 
 Base.eltype(d::SparseDOK{Elt}) where Elt = Elt
 Base.size(d::SparseDOK) = d.sizes;
 SparseDOK{Elt}(::UndefInitializer,sizes::Vararg{Int64,N}) where {Elt,N} = SparseDOK(Dict{NTuple{N,Int64},Elt}(),tuple(sizes...));
+function Base.Array(d::SparseDOK{Elt,N}) where {Elt,N}
+    f = zeros(Elt,d.sizes...);
+    for (k,v) in d.eldict
+        f[k...] = v
+    end
+    f
+end
+Base.getindex(d::SparseDOK{Elt,N},i::Vararg{Int64,N}) where {Elt,N} = get(d.eldict,tuple(i...),zero(Elt));
+Base.setindex!(d::SparseDOK{Elt,N},v::Number,i::Vararg{Int64,N}) where {Elt,N} =  d.eldict[tuple(i...)] = v;
 
-function LinearAlgebra.lmul!(a, d::SparseDOK{Elt,N}) where {Elt,N}
+function LinearAlgebra.lmul!(a::Number, d::SparseDOK{Elt,N}) where {Elt,N}
     for k in keys(d.eldict)
         d.eldict[k]*=a;
     end
 end
-LinearAlgebra.rmul!(d::SparseDOK{Elt,N},a) where {Elt,N} = LinearAlgebra.lmul!(a,d)
-
+LinearAlgebra.rmul!(d::SparseDOK{Elt,N},a::Number) where {Elt,N} = LinearAlgebra.lmul!(a,d)
 
 TensorOperations.similar_from_indices(T::Type, p1, p2, A::SparseDOK, CA::Symbol) = SparseDOK{T}(undef,[A.sizes...][p1...,p2...]...);
 TensorOperations.similar_from_indices(T::Type, poA::Tuple{Vararg{Int64,N}} where N, poB::Tuple{Vararg{Int64,N}} where N,
                                 p1::Tuple{Vararg{Int64,N}} where N, p2::Tuple{Vararg{Int64,N}} where N,
                                 A::SparseDOK, B::SparseDOK, CA::Symbol, CB::Symbol) = SparseDOK{T}(undef,[[A.sizes...][poA...];[B.sizes...][poB...]][[p1...,p2...]]...);
 
-#=
-Implements C = β*C+α*permute(op(A)) where A is permuted such that the left (right)
- indices of C correspond to the indices indleft (indright) of A,
- and op is conj if conjA == :C or the identity map if conjA == :N (default).
- Together, (indleft..., indright...) is a permutation of 1 to the number of indices (dimensions) of A.
-=#
+
 function TensorOperations.add!(α, A::SparseDOK{Elt}, conjA::Symbol, β, C::SparseDOK{Elt}, indleft, indright) where Elt
     LinearAlgebra.lmul!(β,C);
 
@@ -44,39 +43,14 @@ function TensorOperations.add!(α, A::SparseDOK{Elt}, conjA::Symbol, β, C::Spar
     C
 end
 
-#=
-Implements C = β*C+α*partialtrace(op(A)) where A is permuted and partially traced,
-such that the left (right) indices of C correspond to the indices indleft (indright) of A,
-and indices cindA1 are contracted with indices cindA2. Furthermore, op is conj if conjA == :C
-or the identity map if conjA=:N (default). Together, (indleft..., indright..., cind1, cind2)
-is a permutation of 1 to the number of indices (dimensions) of A.
-=#
 function TensorOperations.trace!(α, A::SparseDOK, conjA::Symbol, β, C::SparseDOK,
     indleft, indright, cind1, cind2)
     throw(ArgumentError("not yet implement"))
 end
 
-#=
-Implements C = β*C+α*contract(opA(A),opB(B)) where A and B are contracted,
-such that the indices cindA of A are contracted with indices cindB of B.
-The open indices oindA of A and oindB of B are permuted such that C has left (right) indices
-corresponding to indices indleft (indright) out of (oindA..., oindB...).
-The operation opA (opB) acts as conj if conjA (conjB) equal :C or
-as the identity map if conjA (conjB) equal :N. Together, (oindA..., cindA...) is
-a permutation of 1 to the number of indices of A and (oindB..., cindB...) is a permutation
-of 1 to the number of indices of C. Furthermore, length(cindA) == length(cindB),
-length(oindA)+length(oindB) equals the number of indices of C and (indleft..., indright...) i
-s a permutation of 1 ot the number of indices of C.
-
-The final argument syms is optional and can be either nothing,
-or a tuple of three symbols,
-which are used to identify temporary objects in the cache to be used for
-permuting A, B and C so as to perform the contraction as a matrix multiplication.
-=#
-
-function TensorOperations.contract!(α, A::SparseDOK{Elt}, conjA::Symbol, B::SparseDOK, conjB::Symbol, β, C::SparseDOK,
-    oindA, cindA, oindB, cindB,
-    indleft, indright, syms) where Elt
+function TensorOperations.contract!(α, A::SparseDOK{Elt} where N, conjA::Symbol, B::SparseDOK{Elt}, conjB::Symbol, β, C::SparseDOK{Elt},
+    oindA::Tuple{Vararg{Int}}, cindA::Tuple{Vararg{Int}}, oindB::Tuple{Vararg{Int}}, cindB::Tuple{Vararg{Int}},
+    indleft::Tuple{Vararg{Int}}, indright::Tuple{Vararg{Int}}, syms) where Elt
     LinearAlgebra.lmul!(β,C);
 
     for (kA,vA) in A.eldict
@@ -93,6 +67,9 @@ function TensorOperations.contract!(α, A::SparseDOK{Elt}, conjA::Symbol, B::Spa
 end
 
 A = SparseDOK{Float64}(undef,2,2,3);
-A.eldict[(1,1,1)] = 1;
+A[1,1,1] = 1;
 @tensor v[-1;-2]:=A[1,2,-2]*A[1,2,-1]
-@show v.eldict
+
+fA = Array(A);
+@tensor fv[-1;-2]:=fA[1,2,-2]*fA[1,2,-1]
+@show LinearAlgebra.norm(collect(v)-fv)
--- torun.jl
+++ torun.jl
@@ -4,26 +4,14 @@ matlab2gt(m,n) = SUNIrrep((m+n,n,0))
 let
 
 include("sector.jl")
-#=
-L1 = TensorMap(ones,ComplexF64,RepresentationSpace((SUNIrrep((0,0))=>1))*RepresentationSpace((SUNIrrep((2,0))=>1)),RepresentationSpace((SUNIrrep((2,0))=>1))*RepresentationSpace((SUNIrrep((2,0))=>1)));
-R1 = TensorMap(ones,ComplexF64,RepresentationSpace((SUNIrrep((2,0))=>1))*RepresentationSpace((SUNIrrep((2,0))=>1)),RepresentationSpace((SUNIrrep((0,0))=>1))*RepresentationSpace((SUNIrrep((2,0))=>1)));
-ham1 =  MPOHamiltonian([L1,R1]);
 
-st1 = InfiniteMPS([RepresentationSpace((SUNIrrep((2,0))=>1))],[RepresentationSpace((SUNIrrep((1,0))=>4,SUNIrrep((3,0))=>4,SUNIrrep((5,0))=>4))]);
-(gs1,_) = find_groundstate(st1,ham1,Vumps());
-@show expectation_value(gs1,ham1);
-
-L2 = TensorMap(ones,ComplexF64,ℂ[SU₂](0=>1)*ℂ[SU₂](1=>1),ℂ[SU₂](1=>1)*ℂ[SU₂](1=>1));
-R2 = TensorMap(ones,ComplexF64,ℂ[SU₂](1=>1)*ℂ[SU₂](1=>1),ℂ[SU₂](0=>1)*ℂ[SU₂](1=>1));
-ham2 =  MPOHamiltonian([L2,R2]);
-
-st2 = InfiniteMPS([ℂ[SU₂](1=>1)],[ℂ[SU₂](1//2=>4,3//2=>4,5//2=>4)]);
-(gs2,_) = find_groundstate(st2,ham2,Vumps());
-@show expectation_value(gs2,ham2);
-=#
-a = TensorMap(rand,ComplexF64,RepresentationSpace((matlab2gt(1,1) => 3))*RepresentationSpace((matlab2gt(1,1) => 3))*RepresentationSpace((matlab2gt(0,1) => 3)),RepresentationSpace((matlab2gt(1,1) => 3))*RepresentationSpace((matlab2gt(2,0) => 3)))
-@show norm(a);
-a = permute(a,(3,2,5),(1,4));
-@show norm(a);
-
+out1 = (SUNIrrep{3}((1, 0, 0)), SUNIrrep{3}((1, 1, 0)))
+in1 = SUNIrrep{3}((0, 0, 0))
+out2 = (SUNIrrep{3}((1, 1, 0)), SUNIrrep{3}((1, 0, 0)), SUNIrrep{3}((1, 1, 0)))
+in2 = SUNIrrep{3}((2, 0, 0))
+for f1 = fusiontrees((out1..., dual(in1)), one(in1)),f2 = fusiontrees(out2, in2)
+    for c in f1.coupled ⊗ f2.coupled
+        FusionTree((f1.coupled, f2.coupled), c, (false, false), ())
+    end
+end
 end
