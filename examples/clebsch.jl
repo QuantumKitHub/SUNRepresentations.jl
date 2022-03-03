53f3321860786c83bb8e3fd82d2c599631daa407
maartenvd <maarten.vandamme@ugent.be>
2022-03-03 11:32:41 +0100

    Update clebsch.jl

--- examples/clebsch.jl
+++ examples/clebsch.jl
@@ -8,10 +8,3 @@ b = SUNIrrep(2,1,0);
 c = SUNIrrep(2,1,0);
 
 CGC(a,b,c)
-
-let
-
-d = SUNRepresentations.CGC(Irrep(2,1,0),Irrep(2,1,0),Irrep(2,1,0));
-@tensor temp[-1 -2;-3 -4] := d[1 2 -1 -2]*conj(d[1 2 -3 -4])
-
-end
