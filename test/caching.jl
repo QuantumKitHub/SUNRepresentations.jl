5e6b5db0628804ccf643cdcca827a96ad7e1da53
lkdvos <lukas.devos@ugent.be>
2023-11-28 10:55:23 +0100

    Lower test requirements

--- test/caching.jl
+++ test/caching.jl
@@ -1,17 +1,18 @@
 # Tests for caching of Clebsch-Gordan coefficients
 import SUNRepresentations: cache_info, precompute_disk_cache, cache_path, offsets_path, clear_disk_cache!
 
-for N in 3:5
+for N in 3:4
     cache_info()
     precompute_disk_cache(N, 1)
     @test isfile(cache_path(N))
     @test isfile(offsets_path(N))
+    cache_info()
     clear_disk_cache!(N)
     @test !isfile(cache_path(N))
     @test !isfile(offsets_path(N))
 end
 
-for N in 3:5
-    precompute_disk_cache(N, 2)
+for N in 3:4
+    precompute_disk_cache(N, 1)
 end
 cache_info()
