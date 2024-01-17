01e5a98a2d2dd95a605bafd354c04e54eb833def
lkdvos <lukas.devos@ugent.be>
2024-01-17 16:55:27 +0100

    Add stub test for `cache_info()` `clear_disk_cache!()`

--- test/caching.jl
+++ test/caching.jl
@@ -11,10 +11,15 @@ if get(ENV, "CI", false) == "true"
     clear_disk_cache!()
 end
 
-L = length(sprint(cache_info))
+L = length(sprint(SUNRepresentations.cache_info))
 for N in 3:4
     precompute_disk_cache(N, 1)
     L′ = length(sprint(cache_info))
     @test L′ >= L
     global L = L′
+end
+
+if get(ENV, "CI", false) == "true"
+    println("Detected running on CI")
+    clear_disk_cache!(3, Float64)
 end
