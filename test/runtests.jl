e00677b28320a15d5fb6f009a4d35738cb5d87d9
Boris De Vos <boris.devos@ugent.be>
2026-05-31 13:41:14 +0200

    fix to test higher-dimensional irreps

--- test/runtests.jl
+++ test/runtests.jl
@@ -29,13 +29,13 @@ include(testsuite_path)
 using .SectorTestSuite: randsector, smallset
 
 function SectorTestSuite.smallset(::Type{I}) where {I <: SUNIrrep}
-    N = rank(I)
+    N = SUNRepresentations.rank(I)
     return smallset(I, 5, 10 * N)
 end
 function SectorTestSuite.smallset(::Type{ProductSector{Tuple{I1, I2}}}) where {I1 <: SUNIrrep, I2 <: SUNIrrep}
     s1 = smallset(I1)
     s2 = smallset(I2)
-    N = rank(I1)
+    N = SUNRepresentations.rank(I1)
     return resize!(shuffle!([a ⊠ b for a in s1 for b in s2 if dim(a) * dim(b) <= 100 * N]), 5)
 end
 
